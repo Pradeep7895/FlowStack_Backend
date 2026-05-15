08db9b0f55c83e5c22612528b6197ba66cdf6092
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-05-15 12:51:55 +0530

    [Pradeep] Refactored.Auth Service

--- FlowStack.AuthService/Program.cs
+++ FlowStack.AuthService/Program.cs
@@ -199,15 +199,14 @@ app.UseCookiePolicy(new CookiePolicyOptions
 });
 app.UseGlobalExceptionHandler();
 
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlowStack Auth v1");
-        c.RoutePrefix = "swagger";
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlowStack Auth v1");
+    c.RoutePrefix = "swagger";
+});
+
 
 app.UseCors("AllowFlowStackWeb");
 
