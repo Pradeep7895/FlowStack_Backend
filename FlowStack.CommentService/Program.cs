bfbe1f6e001d6728254af51cf273ba70ee02a632
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-05-15 16:43:59 +0530

    [Pradeep] Refactored. Comment Service

--- FlowStack.CommentService/Program.cs
+++ FlowStack.CommentService/Program.cs
@@ -133,15 +133,13 @@ try
     app.UseGlobalExceptionHandler();
     app.UseRequestLogging();
 
-    if (app.Environment.IsDevelopment())
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlowStack Comment Service v1");
-            c.RoutePrefix = "swagger";
-        });
-    }
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlowStack Comment Service v1");
+        c.RoutePrefix = "swagger";
+    });
+    
 
     app.UseHttpsRedirection();
     app.UseCors("AllowFlowStackWeb");
